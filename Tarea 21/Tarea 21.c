ee892d3bdacd7c334d7079c1702075b90932d388
FranciscoLuna080499 <31667071+FranciscoLuna080499@users.noreply.github.com>
2017-11-14 12:29:44 -0600

    Add files via upload

---
+++ Tarea 21/Tarea 21.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "Tarea21.h"
+
+int main(int argc, char *argv[]) {
+	
+    fnLetraF();
+    fnLetraR();
+    fnLetraA();
+    fnLetraN();
+    fnLetraC();
+    fnLetraI();
+    fnLetraS();
+    fnLetraC();
+    fnLetraO();
+
+	return 0;
+}
---
+++ Tarea 21/Tarea21.h
@@ -0,0 +1,56 @@
+void fnLetraF(){
+        printf("F F F F \n");
+		printf("F \n");
+        printf("F F F \n");
+        printf("F \n");
+        printf("F \n");
+}
+void fnLetraR(){
+           printf("R R R R \n");
+           printf("R     R \n");
+           printf("R R R R \n");
+           printf("R   R \n");
+           printf("R     R \n");
+}
+void fnLetraA(){
+           printf("A A A A \n");
+           printf("A     A\n");
+           printf("A A A A \n");
+           printf("A     A \n");
+           printf("A     A \n");
+}
+void fnLetraN(){
+           printf("N    N \n");
+           printf("N N  N \n");
+           printf("N  N N \n");
+           printf("N    N \n");
+           printf("N    N \n");
+}
+void fnLetraC(){
+           printf("C C C C \n");
+           printf("C \n");
+           printf("C \n");
+           printf("C \n");
+           printf("C C C C \n");
+}
+void fnLetraI(){
+           printf("I I I I \n");
+           printf("   I \n");
+           printf("   I \n");
+           printf("   I \n");
+           printf("I I I I \n");
+}
+void fnLetraS(){
+           printf("S S S S \n");
+           printf("S \n");
+           printf("S S S S \n");
+           printf("      S \n");
+           printf("S S S S \n");
+}
+void fnLetraO(){
+           printf("O O O O \n");
+           printf("O     O \n");
+           printf("O     O \n");
+           printf("O     O \n");
+           printf("O O O O \n");   
+}
