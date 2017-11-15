78ac46771181658e59bdf85913e5f648573fa14b
FranciscoLuna080499 <31667071+FranciscoLuna080499@users.noreply.github.com>
2017-11-15 13:25:04 -0600

    Add files via upload

---
+++ Tarea 22/2do_Programa.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "info.h"
+
+
+int main(int argc, char *argv[]) {
+	appInfoData("Ejemplo de Punteros", "14/11/2017");
+	
+	int a;
+	a=10;
+	int *puntero;
+	puntero=&a;
+	
+	printf("%d \n", *puntero);
+	printf("%d \n", puntero);
+	printf("%d \n", a);
+	printf("%d \n", &a);
+	
+	
+	return 0;
+}
---
+++ Tarea 22/info.h
@@ -0,0 +1,10 @@
+void appInfoData(char *titulo, char *fecha)
+{
+  printf("----------------------------------------------------------------\n");
+  printf("_\n");
+  printf("_       %s\n", titulo);
+  printf("_       Francisco Alberto Luna Velazquez ®\n");
+  printf("_       %s\n", fecha);
+  printf("_\n");
+  printf("----------------------------------------------------------------\n");
+}
